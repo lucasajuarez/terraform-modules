3d0eeeec088a9bf87359d6b3e42e27d03ad4b631
lucas juarez <lucas.juarez@boreal-it.com>
2025-01-04 12:24:07 -0300

    Init Branch

--- ecs/cluster/outputs.tf
+++ ecs/cluster/outputs.tf
@@ -1,4 +1,8 @@
 # Output del ID de la VPC creada
 output "cluster_arn" {
-    value       = module.ecs_cluster.arn
+  value = module.ecs_cluster.arn
+}
+
+output "cluster_name" {
+  value = module.ecs_cluster.name
 }
