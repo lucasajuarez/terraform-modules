1eacfd123825e6959bb11c718fbd8a793a1c1c21
lucas juarez <lucas.juarez@boreal-it.com>
2025-01-06 02:17:13 -0300

    Init Branch

---
+++ secrets_manager/variables.tf
@@ -0,0 +1,31 @@
+variable "name_prefix" {
+  description = "Prefix for the secret name"
+  type        = string
+}
+
+variable "description" {
+  description = "Description for the secret"
+  type        = string
+}
+
+variable "recovery_window_in_days" {
+  description = "Number of days to retain the secret after deletion"
+  type        = number
+  default     = 30
+}
+
+variable "secret_values" {
+  description = "Key-value pairs to store in the secret"
+  type        = map(string)
+}
+
+variable "ecs_service_role_arn" {
+  description = "ARN of the ECS IAM role that needs access to the secret"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags to apply to the secret"
+  type        = map(string)
+  default     = {}
+}
