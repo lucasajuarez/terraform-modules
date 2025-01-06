d4d8ee3e4138d891c93e923e39d3ee2e5b3a99e6
lucas juarez <lucas.juarez@boreal-it.com>
2025-01-06 00:26:58 -0300

    Init Branch

--- ecs/service/main.tf
+++ ecs/service/main.tf
@@ -1,4 +1,4 @@
-module "ecs_service" {
+module "ecs-service" {
   source = "terraform-aws-modules/ecs/aws//modules/service"
 
   name        = var.name
@@ -14,51 +14,50 @@ module "ecs_service" {
   task_exec_iam_role_arn = var.task_role
   tasks_iam_role_arn = var.task_role
 
+  container_definitions = {
+    (var.container_name) = {
+      cpu       = 512
+      memory    = 1024
+      essential = true
+      image     = tostring(var.apiImage)
+      port_mappings = [
+        {
+          name          = "apiport"
+          containerPort = var.apiPort
+          protocol      = "tcp"
+        }
+      ]
+      memory_reservation = 50
+    }
+  }
+
+  subnet_ids = var.subnet_ids
+
+  security_group_rules = {
+    alb_ingress_3000 = {
+      type                     = "ingress"
+      from_port                = 0
+      to_port                  = 0
+      protocol                 = "-1"
+      cidr_blocks = ["0.0.0.0/0"]    }
+    egress_all = {
+      type        = "egress"
+      from_port   = 0
+      to_port     = 0
+      protocol    = "-1"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  service_connect_configuration = {
+    namespace = var.namespace
+    service = {
+      client_alias = {
+        port     = var.apiPort
+        dns_name = "${var.name}-${var.apiPort}.${var.namespace}"
+      }
+      port_name      = "apiport"
+      discovery_name = "${var.name}-${var.apiPort}-tcp"
+    }
+  }
 }
-#   # container_definitions = {
-#   #   (var.container_name) = {
-#   #     cpu       = 512
-#   #     memory    = 1024
-#   #     essential = true
-#   #     image     = tostring(var.apiImage)
-#   #     port_mappings = [
-#   #       {
-#   #         name          = "apiport"
-#   #         containerPort = var.apiPort
-#   #         protocol      = "tcp"
-#   #       }
-#   #     ]
-#   #     memory_reservation = 50
-#   #   }
-#   # }
-
-#   subnet_ids = var.subnet_ids
-
-#   security_group_rules = {
-#     alb_ingress_3000 = {
-#       type                     = "ingress"
-#       from_port                = 0
-#       to_port                  = 0
-#       protocol                 = "-1"
-#       cidr_blocks = ["0.0.0.0/0"]    }
-#     egress_all = {
-#       type        = "egress"
-#       from_port   = 0
-#       to_port     = 0
-#       protocol    = "-1"
-#       cidr_blocks = ["0.0.0.0/0"]
-#     }
-#   }
-
-#   service_connect_configuration = {
-#     namespace = var.namespace
-#     service = {
-#       client_alias = {
-#         port     = var.apiPort
-#         dns_name = "${var.name}-${var.apiPort}.${var.namespace}"
-#       }
-#       port_name      = "apiport"
-#       discovery_name = "${var.name}-${var.apiPort}-tcp"
-#     }
-#   }
-# }
