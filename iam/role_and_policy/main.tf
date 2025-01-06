8b40339cc74e2287b4b623aff69765c5c7fe909b
lucas juarez <lucas.juarez@boreal-it.com>
2025-01-05 23:48:12 -0300

    Init Branch

--- iam/role_and_policy/main.tf
+++ iam/role_and_policy/main.tf
@@ -13,11 +13,6 @@ module "ecs_execution_role" {
     module.iam_policy.arn
   ]
 
-output "ecs_execution_role_arn" {
-    description = "The ARN of the ECS execution role"
-    value = module.ecs_execution_role.iam_role_arn
-}  
-
 }
 
 
@@ -28,4 +23,9 @@ module "iam_policy" {
   path        = "/"
   description = "task Execution Policy"
   policy = var.policy
+}
+
+output "ecs_execution_role_arn" {
+  description = "The ARN of the ECS execution role"
+  value       = module.ecs_execution_role.iam_role_arn
 }
--- iam/role_and_policy/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "ecs_execution_role_arn" {
-    description = "The ARN of the ECS execution role"
-    value = module.ecs_execution_role.iam_role_arn
-}
