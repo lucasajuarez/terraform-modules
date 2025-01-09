7712b32df505e97d01a7ae3d8be21f7cedd9ab4b
lucas juarez <lucas.juarez@boreal-it.com>
2025-01-09 18:08:28 -0300

    Init Branch

--- load_balancer/applicationHTTPS/outputs.tf
+++ load_balancer/applicationHTTPS/outputs.tf
@@ -1,3 +1,8 @@
 output "dns_name" {
  value = module.alb.dns_name
 }
+
+output "target_group_arns" {
+  value = [for tg in module.alb.var.target_groups : tg["arn"]]
+  description = "List of ARNs for the dynamically created Target Groups"
+}
