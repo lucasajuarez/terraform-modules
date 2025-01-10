f8b07d56a8df8ee3478bf8aa9f1a97e1b766380d
lucas juarez <lucas.juarez@boreal-it.com>
2025-01-10 09:04:03 -0300

    Init Branch

--- load_balancer/applicationHTTPS/outputs.tf
+++ load_balancer/applicationHTTPS/outputs.tf
@@ -4,10 +4,6 @@ output "dns_name" {
 
 output "target_group_arns" {
   #value = [for tg in module.alb.target_groups : tg["arn"]]
-  value = { for tg in module.alb.target_groups : tg["name"] => tg["arn"] }
+  value = { for tg in module.alb.target_groups : tg["name"] => tg["arn"] }["oauth2"]
   description = "List of ARNs for the dynamically created Target Groups"
-}
-
-output "oauth2_arn" {
-    value = output.target_group_arns["oauth2"]
 }
