73c41df9eb0a9850099724ca862da6964f3ede21
lucas juarez <lucas.juarez@boreal-it.com>
2025-01-13 10:34:05 -0300

    Init Branch

--- s3/bucket/main.tf
+++ s3/bucket/main.tf
@@ -2,7 +2,7 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
   bucket = var.bucket_name
-  acl    = "private"
+  acl    = "public-read"
 
   control_object_ownership = true
   object_ownership         = "ObjectWriter"
