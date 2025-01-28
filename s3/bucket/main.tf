cc585a4841625c20835ffb04793c5d60ea05ce96
lucas juarez <lucas.juarez@boreal-it.com>
2025-01-27 21:39:23 -0300

    Redirect HTTP ALB

--- s3/bucket/main.tf
+++ s3/bucket/main.tf
@@ -2,7 +2,8 @@ module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
   bucket = var.bucket_name
-  acl    = "private"
+  #acl    = "private"
+  block_public_acls = true
 
   control_object_ownership = true
   object_ownership         = "ObjectWriter"
