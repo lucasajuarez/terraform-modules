e04a821d13109e0395701030049e1cdb319c97f0
lucas juarez <lucas.juarez@boreal-it.com>
2025-01-29 19:18:52 -0300

    Redirect HTTP ALB

--- s3/bucket/main.tf
+++ s3/bucket/main.tf
@@ -5,6 +5,7 @@ module "s3_bucket" {
   acl    = "private"
   block_public_acls = false
   block_public_policy     = false
+  ignore_public_acls      = false
   restrict_public_buckets = false
 
   control_object_ownership = true
