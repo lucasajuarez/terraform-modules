bf19838ae8c61f931467eeb3253bbc83bb89a8c1
lucas.juarez <juarezlucas.92@gmail.com>
2025-03-11 21:29:42 -0300

    Remove account name variable in bucket policy

--- s3/bucket_policy/variables.tf
+++ s3/bucket_policy/variables.tf
@@ -11,12 +11,7 @@ variable "s3_bucket_arn" {
 variable "ecs_role_arn" {
     description = "AWS ECS Role Principal"
     type        = string
-    }
-
-variable "account_name" {
-    description = "Numero de cuenta de AWS"
-    type        = string
-    }        
+    }  
 
 variable "sparkle_s3_user_arn" {
     description = "Sparkle Role AWS Principal"
